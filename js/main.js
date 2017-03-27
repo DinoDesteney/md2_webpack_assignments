087bbe9c1a0b1e8fad2509676d92c677c781573f
DinoDesteney <desbickel@hotmail.com>
2017-03-27 13:43:15 +0200

    iets

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
 /**
  * Opdracht 1: primitives
  * Maak voor de volgende primitives 5 goed bedachte variabelen per primitive
- * 
+ *
  * 5 variabelen met daarin een string
  * 5 variabelen met daarin een number
  * 5 variabelen met daarin een boolean
@@ -17,7 +17,17 @@
  * van Media Development producten (apps, sites, etc.)
  * Bijvoorbeeld een string variabele met de naam 'teacherName' en als waarde 'Berend'
  */
+var Class = 'M&C';
+var Class1 = 'Com';
+var Class2 = 'Hi';
 
+var classroom = "1.66";
+var classroom1 = "1.52";
+var classroom2 = "1.11";
+
+var Liking = true;
+var Liking1 = false;
+var Liking2 = false;
 /**
  * Opdracht 2: Arrays
  * Bedenk 3 arrays en vul ze met primitives
@@ -25,41 +35,60 @@
  *
  * Bijvoorbeeld een Array met de naam 'students' en als waarde ['Berend', "Erwin']
  */
-
+var animals = ["Dogs", "Cats", "Horse"];
+console.log (animals);
 /**
  * Opdracht 3: Arrays push
  * Maak een lege array aan
  * push er een string in
  */
-
+var cookies = [];
+cookies.push('choco', 'strawberry');
+console.log (cookies);
 /**
  * Opdracht 4: Array splice
  * Maak een array aan en vul hem met namen
  * Gooi de naam op de 1e plek weg
  * Gooi de naam op de 3e plek weg
  */
-
+var candy = ["strawberry", "smurf", "cola", "lolly"];
+console.log(candy, "Dit is de array zonder splicing")
+candy.splice(0,1);
+candy.splice(1,1);
+console.log(candy);
 /**
  * Opdracht 5: Objects
  * Maak van 10 objecten uit je dagelijkse leven een object
  *
  * Bijvoorbeeld een object met de gegevens van je mobiel zoals welk merk, type en hoe oud hij is
  */
-
+var mobile = {
+    Mobilebrand : "LG",
+    MobileCompany: "LG",
+    Price: "300",
+    Color: "Black & White",
+    Age: "2 years",
+    Status: "Broke as fuck"
+};
 /**
  * Opdracht 6: Functions
  * Bedenk 4 functies die handig zouden kunnen zijn
  * Bijvoorbeeld een functie met de naam 'multiply'. Deze functie verwacht 2 parameters value & multiplier
  * console.log( value * multiplier)
  */
-
+function multiply(value, multiplier){
+    return value*multiplier;
+}
+console.log(multiply(6,6));
 /**
  * Opdracht 7: Function & objects
  * Maak een functie die een firstName en lastName verwacht als parameter
  * vervolgens returned de functie een object met daarin de firstname en lastname opgeslagen
  * roep deze functie aan en console.log() de waarde
  */
+function names(firstname, lastname) {
 
+}
 /**
  * Opdracht 8: Functions, objects
  * Maak een object genaamd 'student'
